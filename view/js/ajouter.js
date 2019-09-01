9b3799daad97175b874bb3c672d8a7f92bb26adb
Gaetan Jonathan <gaetan.s118@gmail.com>
2019-09-01 14:17:04 +0300

    pret a pousser

--- view/js/ajouter.js
+++ view/js/ajouter.js
@@ -1,11 +1,8 @@
 function ajout(){
-  alert('allo');
   eel.ajouter_verre(document.getElementById('atype').value, document.getElementById('atraitement').value, document.getElementById('adegre').value, document.getElementById('anombre').value , document.getElementById('aachat').value, document.getElementById('avente').value )(feed_ajout);
-  alert('allo again');
-
 }
 
 
 function feed_ajout(response){
-  console.log(response);
+  alert('success');
 }
