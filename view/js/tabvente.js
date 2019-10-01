698c52803dd79d40b66126f3b955626dac002143
Gaetan Jonathan BAKARY <gaetan.jonathan.bakary@esti.mg>
2019-10-01 10:56:02 +0300

    monture evenement 3eme forme normale fait

--- view/js/tabvente.js
+++ view/js/tabvente.js
@@ -8,20 +8,20 @@ $(function(){
 function recvProduct(response){
   // fonction recevant le retour de la fonction python appeler
   monture_product = response;
-  is_yet = new Array();
+  is_yet = new Array()
   for (i=0;i<response[0].length;i++){
-    $('#vente_monture #reference').append('<option>' + response[0][i][1] + '</option>');
+    ref = response[0][i][1];
+    if (is_yet.indexOf(ref) < 0){
+      $('#vente_monture #reference').append('<option>' + response[0][i][1] + '</option>');
+      is_yet.push(ref);
+    }
     //$('#vente_monture #couleur').append('<option>' + response[0][i][2] + '</option>');
     //$('#vente_monture #forme').append('<option value='+ response[0][i][3] + '>');
   }
   $('#vente_monture #nombre').html('<option>0</option>'); // on  remplace directement
 
   for (i=0;i<response[1].length;i++){
-    if !(response[1][i][1] in is_yet){
-      $('#vente_verre #type').append('<option>' + response[1][i][1] + '</option>');
-      is_yet.push(response[1][i][1])
-    }
-
+    $('#vente_verre #type').append('<option>' + response[1][i][1] + '</option>');
     //$('#vente_verre #traitement').append('<option>' + response[1][i][2] + '</option>');
     //$('#vente_verre #degre').append('<option>' + response[1][i][3] + '</option>');
   }
@@ -31,14 +31,40 @@ function recvProduct(response){
 
 
 function refChange(obj){
-  if (obj == 'monture'){
-      var val = ($("#vente_monture input[list='reference']").val());
-
+  if (obj == 'reference'){
+      var valRef = $("#vente_monture input[list='reference']").val();
+      $('#vente_monture #forme').html('')
       for (i=0;i<monture_product[0].length;i++){
-        if (response[0][i][1] == val ){
-          $('#vente_monture #forme').append('<option value='+ response[0][i][3] + '>');
-      }
+        if (monture_product[0][i][1] == valRef ){
+          $('#vente_monture #forme').append('<option value='+ monture_product[0][i][3] + '>');
+        }
       }
   }
+  else if (obj == 'forme'){
+    var valRef = $("#vente_monture input[list='reference']").val();
+    var valFor = $("#vente_monture input[list='forme']").val();
+    for (i=0;i<monture_product[0].length;i++){
+      if (monture_product[0][i][1] == valRef && monture_product[0][i][3] == valFor){
+        $('#vente_monture #couleur').append('<option value='+ monture_product[0][i][2] + '>');
 
+      }
+    }
+  }
+
+  else if (obj == 'couleur'){
+    var valRef = $("#vente_monture input[list='reference']").val();
+    var valFor = $("#vente_monture input[list='forme']").val();
+    var valCol = $("#vente_monture input[list='couleur']").val();
+    for (i=0;i<monture_product[0].length;i++){
+      alert(monture_product[0][i][1] == valRef && monture_product[0][i][3] == valFor && monture_product[0][i][2] == valCol);
+      if (monture_product[0][i][1] == valRef && monture_product[0][i][3] == valFor && monture_product[0][i][2] == valCol){
+        $('#vente_monture #nombre').html('');
+        for (j=1;j<=monture_product[0][i][4];j++){
+          $('#vente_monture #nombre').append('<option value=' + j + '>');
+        }
+
+      }
+    }
+
+  }
 }
